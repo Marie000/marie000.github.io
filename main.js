280edc60cf6a54e9e401bed11511cc585165f832
Marie <marie.pelletier@gmail.com>
2016-04-10 20:57:34 -0400

    basic design done, fadeout/fadein working for one page only

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#about').hide();
+	$('#work').hide();
+	$('#contact').hide();
+
+$('#aboutLink').click(function(){
+	$('#home').fadeOut('slow');
+	$('#about').fadeIn('slow');
+})
+
+});
+
+
+
