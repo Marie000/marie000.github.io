320baca002e65aeb79b8ae772a1c37f924ce937f
Marie <marie.pelletier@gmail.com>
2016-04-12 19:42:33 -0400

    sections now fade in and out properly. Added some sample content to the work section.

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
+var fadeOut = function(){
+	var list = ['#home','#about','#work','#contact']
+	for (var i=0;i<list.length;i++){
+		if ($(list[i]).is(':visible')){
+			$(list[i]).fadeOut('slow')
+		}
+	}
+}
+
 $(document).ready(function(){
 	$('#about').hide();
 	$('#work').hide();
 	$('#contact').hide();
 
 $('#aboutLink').click(function(){
-	$('#home').fadeOut('slow');
+	fadeOut();
 	$('#about').fadeIn('slow');
 })
+
+$('#homeLink').click(function(){
+	fadeOut();
+	$('#home').fadeIn('slow');
+})
+
+$('#workLink').click(function(){
+	fadeOut();
+	$('#work').fadeIn('slow');
+})
+
+$('#contactLink').click(function(){
+	fadeOut();
+	$('#contact').fadeIn('slow');
+})
+
 
 });
 
